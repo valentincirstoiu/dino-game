102c1b3661dee73d5cd573e9ce4edf8ebb6b6e05
valentincirstoiu <valentincirstoiu@gmail.com>
2021-11-25 18:49:45 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+    setTimeout(() => {
+      dino.classList.remove("jump");
+    }, 500);
+  }
+}
+let dinoPlaying = setInterval(() => {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 150) {
+    alert("Game Over!");
+  }
+}, 10);
+
+document.addEventListener("click", function (ev) {
+  jump();
+});
